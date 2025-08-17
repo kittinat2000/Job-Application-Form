1e8fd35be9197a04b83bf4f1dbe63b875dc04db8
kittinat2000 <non15120@gmail.com>
2025-08-18 01:11:25 +0700

    เพิ่มสคลิป

--- user_manage.php
+++ user_manage.php
@@ -64,5 +64,6 @@ $result = $conn->query($sql);
       </table>
     </div>
   </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
